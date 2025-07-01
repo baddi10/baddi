75fe3d182de4cf7499f1c92dc50fd3a771214995
Baddi <cvremo69@gmail.com>
2025-07-01 12:17:38 +0530

    Initial commit

---
+++ src/main/java/com/example/baddi/BaddiApplication.java
@@ -0,0 +1,13 @@
+package com.example.baddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaddiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaddiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/baddi/BaddiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaddiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
